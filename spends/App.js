3bc2895eb23df64bda6690ab14ff958d2893a51a
Supakorn <64070238@kmitl.ac.th>
2023-10-16 14:48:56 +0700

    Update tab bar with icon
    icon and change name

--- spends/App.js
+++ spends/App.js
@@ -1,6 +1,6 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-
+import { AntDesign } from '@expo/vector-icons';
 
 import Tab1 from "../spends/screens/Tab1";
 import Tab2 from "../spends/screens/Tab2";
@@ -8,18 +8,53 @@ import Addrecord from "../spends/screens/Addrecord";
 import Tab4 from "../spends/screens/Tab4";
 import Tab5 from "../spends/screens/Tab5";
 
-
 const Tab = createBottomTabNavigator();
 
 export default function App() {
- return (
-  <NavigationContainer>
-  <Tab.Navigator initialRouteName="T1">
-  <Tab.Screen name= "Tab_1" component={Tab1} />
-  <Tab.Screen name= "Tab_2" component={Tab2} />
-  <Tab.Screen name= "Tab_3" component={Addrecord} />
-  <Tab.Screen name= "Tab_4" component={Tab4} />
-  <Tab.Screen name= "Tab_5" component={Tab5} />
-  </Tab.Navigator>
-  </NavigationContainer>
- ); }
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="T1">
+        <Tab.Screen options={{
+          tabBarIcon: ({ color }) => {
+            return (
+                <AntDesign name="home" size={24} color={color} />
+            );
+          },
+          tabBarOptions: { tabBarActiveTintColor: "blue" },
+        }} name="หน้าหลัก" component={Tab1} />
+        <Tab.Screen options={{
+          tabBarIcon: ({ color }) => {
+            return (
+                <AntDesign name="search1" size={24} color={color} />
+            );
+          },
+          tabBarOptions: { tabBarActiveTintColor: "blue" },
+        }} name="ตรวจสอบ" component={Tab2} />
+        <Tab.Screen options={{
+          tabBarIcon: ({ color }) => {
+            return (
+                <AntDesign name="pluscircleo" size={24} color={color} />
+            );
+          },
+          tabBarOptions: { tabBarActiveTintColor: "blue" },
+        }} name="เพิ่มรายการ" component={Addrecord} />
+        <Tab.Screen options={{
+          tabBarIcon: ({ color }) => {
+            return (
+                <AntDesign name="barschart" size={24} color={color} />
+            );
+          },
+          tabBarOptions: { tabBarActiveTintColor: "blue" },
+        }} name="เปรียบเทียบ" component={Tab4} />
+        <Tab.Screen options={{
+          tabBarIcon: ({ color }) => {
+            return (
+                <AntDesign name="bells" size={20} color={color} />
+            );
+          },
+          tabBarOptions: { tabBarActiveTintColor: "blue" },
+        }} name="การแจ้งเตือน" component={Tab5} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
