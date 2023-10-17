fb211fe716d69c9023f3e707eacdd70e83b257e6
eartsvnh <88610831+S1vanat@users.noreply.github.com>
2023-10-18 00:49:24 +0700

    navbar addrecord tab.screen
    ทำปุ่มบวกให้คล้ายที่ออกแบบไว้

--- spends/App.js
+++ spends/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { AntDesign } from '@expo/vector-icons';
@@ -7,56 +9,86 @@ import Addrecord from "../spends/screens/Addrecord";
 import Tab4 from "../spends/screens/Tab4";
 import Tab5 from "../spends/screens/Tab5";
 
-
 const Tab = createBottomTabNavigator();
-
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{
-        headerStyle: { backgroundColor: "orange" },
-        headerTintColor: "black",
-        tabBarActiveTintColor: "orange",
-        tabBarStyle: { backgroundColor: "black" },
-        headerTitleAlign: 'center'
-      }} initialRouteName="T1">
-        <Tab.Screen options={{
-          tabBarIcon: ({ color }) => {
-            return (
+      <View style={{ flex: 1 }}>
+        <Tab.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "orange" },
+            headerTintColor: "black",
+            tabBarActiveTintColor: "orange",
+            tabBarStyle: { backgroundColor: "black", flexDirection: 'row', justifyContent: 'space-between' },
+            headerTitleAlign: 'center'
+          }}
+          initialRouteName="T1"
+        >
+          <Tab.Screen
+            options={{
+              tabBarIcon: ({ color }) => (
                 <AntDesign name="home" size={24} color={color} />
-            );
-          }
-        }} name="หน้าหลัก" component={Home} />
-        <Tab.Screen options={{
-          tabBarIcon: ({ color }) => {
-            return (
+              ),
+            }}
+            name="หน้าหลัก"
+            component={Home}
+          />
+          <Tab.Screen
+            options={{
+              tabBarIcon: ({ color }) => (
                 <AntDesign name="search1" size={24} color={color} />
-            );
-          }
-        }} name="ตรวจสอบ" component={Tab2} />
-        <Tab.Screen options={{
-          tabBarIcon: ({ color }) => {
-            return (
-                <AntDesign name="pluscircleo" size={24} color={color} />
-            );
-          }
-        }} name="เพิ่มรายการ" component={Addrecord} />
-        <Tab.Screen options={{
-          tabBarIcon: ({ color }) => {
-            return (
+              ),
+            }}
+            name="ตรวจสอบ"
+            component={Tab2}
+          />
+          <Tab.Screen
+            name="เพิ่มรายการ"
+            component={Addrecord}
+            options={{
+              
+              tabBarIcon: ({ color }) => (
+                <View style={styles.iconContainer}>
+                  <AntDesign name="pluscircleo" size={50} color={color} />
+                </View>
+              ),
+            }}
+          />
+          <Tab.Screen
+            options={{
+              tabBarIcon: ({ color }) => (
                 <AntDesign name="barschart" size={24} color={color} />
-            );
-          }
-        }} name="เปรียบเทียบ" component={Tab4} />
-        <Tab.Screen options={{
-          tabBarIcon: ({ color }) => {
-            return (
+              ),
+            }}
+            name="เปรียบเทียบ"
+            component={Tab4}
+          />
+          <Tab.Screen
+            options={{
+              tabBarIcon: ({ color }) => (
                 <AntDesign name="bells" size={20} color={color} />
-            );
-          }
-        }} name="การแจ้งเตือน" component={Tab5} />
-      </Tab.Navigator>
+              ),
+            }}
+            name="การแจ้งเตือน"
+            component={Tab5}
+          />
+        </Tab.Navigator>
+      </View>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    backgroundColor:"black",
+    borderRadius:80,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    bottom: 0,
+    zIndex: 1,
+    width:70,
+    height:70
+  },
+});
